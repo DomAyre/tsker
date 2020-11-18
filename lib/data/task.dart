3a751ce0c322768ed8133633f382b57b192f62e3
Dom Ayre <domayre@outlook.com>
2020-11-18 12:54:15 +0000

    Add task completion

--- lib/data/task.dart
+++ lib/data/task.dart
@@ -1,7 +1,23 @@
+class TaskWorkLog {
+  DateTime start;
+  DateTime end;
+
+  TaskWorkLog({this.start});
+  TaskWorkLog.now() {
+    this.start = DateTime.now();
+  }
+
+  void endLog() {
+    this.end = DateTime.now();
+  }
+}
+
 class Task {
   String title;
   Duration timeLogged = Duration.zero;
+  bool completed = false;
   List<DateTime> scheduledTimes = [];
+  List<TaskWorkLog> workLogs = [];
 
   Task({
     this.title,
@@ -22,4 +38,23 @@ class TaskManager {
           time.month == date.month &&
           time.day == date.day))
       .toList();
+
+  void completeTask(Task task) {
+    task.completed = true;
+  }
+
+  void uncompleteTask(Task task) {
+    task.completed = false;
+  }
+
+  void startTask(Task task) {
+    assert(task.workLogs[-1].end != null);
+    task.workLogs.add(TaskWorkLog.now());
+  }
+
+  void endTask(Task task) {
+    TaskWorkLog lastLog = task.workLogs[-1];
+    assert(lastLog.end == null);
+    lastLog.endLog();
+  }
 }
